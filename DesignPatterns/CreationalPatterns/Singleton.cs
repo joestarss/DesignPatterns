2c9a58eed9c2d3be56619f55acab54b62898a227
joestarss <songsongyang611@gmail.com>
2023-12-05 21:56:51 -0500

    singleton, observer, strategy

--- DesignPatterns/CreationalPatterns/Singleton.cs
+++ DesignPatterns/CreationalPatterns/Singleton.cs
@@ -6,7 +6,41 @@ using System.Threading.Tasks;
 
 namespace DesignPatterns.CreationalPatterns
 {
-    internal class Singleton
+    // Use case: control access to some shared resource, eg. database connnection, logging, configuration settings
+    // Implementations:
+    // 1. Make the default constructor private, to prevent other objects from using the new operator with the Singleton class
+    // 2. Create a static creation method that acts as a constructor
+
+    public class Singleton
     {
+        public string Value { get; set; }
+
+        private static Singleton _instance; // Private static field for storing the singleton instance
+
+        private static readonly object _lock = new object();
+
+        private Singleton() // The Singleton’s constructor should be hidden from the client code
+        {
+            Console.WriteLine("Singleton instance created.");
+        }
+
+        // Public static method to get the singleton instance
+        // Can adjust extra logic here, eg. limitation(more than one of instance)
+        public static Singleton GetInstance(string value)
+        {
+            if (_instance == null)
+            {
+                lock (_lock) // Thread lock for multi-thread support
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                        _instance.Value = value;
+                    }
+                    return _instance;
+                }
+            }
+            return _instance;
+        }
     }
 }
--- DesignPatterns/Program.cs
+++ DesignPatterns/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using DesignPatterns.BehavioralPatterns;
+using DesignPatterns.CreationalPatterns;
 
 namespace DesignPatterns
 {
@@ -9,6 +11,76 @@ namespace DesignPatterns
         static void Main(string[] args)
         {
             Console.WriteLine("DP Sample");
+
+            //TestSingletonMultiThread();
+            //TestObserver();
+            TestStrategy();
         }
-    }   
+
+        #region Singleton
+        public static void TestSingletonMultiThread()
+        {
+            Thread process1 = new Thread(() =>
+            {
+                Singleton instance1 = Singleton.GetInstance("Tread1");
+                Console.WriteLine(instance1.Value);
+                Console.WriteLine($"Instance 1 HashCode: {instance1.GetHashCode()}");
+            });
+            Thread process2 = new Thread(() =>
+            {
+                Singleton instance2 = Singleton.GetInstance("Tread2");
+                Console.WriteLine(instance2.Value);
+                Console.WriteLine($"Instance 2 HashCode: {instance2.GetHashCode()}");
+            });
+
+            process1.Start();
+            process2.Start();
+
+            process1.Join();
+            process2.Join();
+        }
+        #endregion
+
+        #region Observer
+        public static void TestObserver()
+        {
+            // The client code.
+            var subject = new Subject();
+            var observerA = new ConcreteObserverA();
+            subject.Attach(observerA);
+
+            var observerB = new ConcreteObserverB();
+            subject.Attach(observerB);
+
+            subject.SomeBusinessLogic();
+            subject.SomeBusinessLogic();
+
+            subject.Detach(observerB);
+
+            subject.SomeBusinessLogic();
+        }
+        #endregion
+
+        #region Strategy
+        public static void TestStrategy()
+        {
+            // The client code picks a concrete strategy and passes it to the
+            // context. The client should be aware of the differences between
+            // strategies in order to make the right choice.
+            var context = new Context();
+
+            Console.WriteLine("Client: Strategy is set to normal sorting.");
+            context.SetStrategy(new ConcreteStrategyA());
+            context.DoSomeBusinessLogic();
+
+            Console.WriteLine();
+
+            Console.WriteLine("Client: Strategy is set to reverse sorting.");
+            context.SetStrategy(new ConcreteStrategyB());
+            context.DoSomeBusinessLogic();
+        }
+        #endregion
+    }
+
+
 }
